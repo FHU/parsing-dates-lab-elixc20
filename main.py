0834b050767578219aa70986f5e1f51f6bb1820c
elixc20 <elicombs2020@gmail.com>
2024-02-08 19:06:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,20 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 #parse_date function should return the date formated as MM/DD/YYYY
 #DO NOT REMOVE THIS FUNCTION
-def parse_month(month):
-    month_dict = {"January": "01", "February": "02", 'March' : "03", "April": "04", "May": "05", "June": "06", "July": "07", "August": "08", "September": "09", "October": "10", 'November': "11", "December": "12"}
-    return months[month]
 
-def parse_date(orig_date):
-        
-    next_date = orig_date.split(',')
-    next_date = next_date.split('')
+def parse_date(string_date):
     
-    new_month = parse_month(next_date[0])
-    new_day = next_date[1]
-    new_year = next_date[2]
-
-    def parse_new_date(new_month, new_day, new_year):
-        final_date = f'{new_month:02d}/{new_day:02d}/{new_year}'
-
+    if string_date != "-1":
+        month, day, year = string_date.split(' ')
+        day = int(day.replace(",", ""))
+        day = '{:02d}'.format(day)
+        month_dictionary = {"January": "01", "February": "02", 'March' : "03", "April": "04", "May": "05", "June": "06", "July": "07", "August": "08", "September": "09", "October": "10", 'November': "11", "December": "12" }
+        month = month_dictionary.get(month,"01")
+        separator = '/'
+        reg_date = separator.join((month, day, year))
+        return reg_date
+    
 if __name__ == '__main__':
-    final_date = input()
-    parse_date(final_date)
-    print(parse_date(final_date))
+    string_date = input()
+    print(reg_date)
     
